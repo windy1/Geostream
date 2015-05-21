6ab07a2dc15abbb5afeb5cecc9cc6cbdfe2994a1
Walker Crouse <walkercrouse@hotmail.com>
2015-05-20 20:01:54 -0400

    Let's build the json in the constructor instead
    Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

--- app/src/main/java/se/walkercrou/geostream/net/response/ApiResponse.java
+++ app/src/main/java/se/walkercrou/geostream/net/response/ApiResponse.java
@@ -5,38 +5,49 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import se.walkercrou.geostream.util.AppUtil;
 
 /**
- * Represents a response from the server's RESTful API.
+ * Represents a response from the server's RESTful API. Can return either a {@link JSONObject} or
+ * {@link JSONArray}.
  */
 public class ApiResponse extends Response {
     public static final String ERROR_DETAIL = "detail";
-    private String jsonString;
+    private JSONObject obj;
+    private JSONArray array;
 
     public ApiResponse(int statusCode, String statusMessage, InputStream in) {
         super(statusCode, statusMessage, in);
 
-        // body as string
         StringBuilder builder = new StringBuilder();
         BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
         try {
+            // read response
             String ln;
             while ((ln = reader.readLine()) != null)
                 builder.append(ln).append('\n');
 
-            // close the reader and input stream
+            // close reader and input stream
             reader.close();
             in.close();
-        } catch (Exception e) {
-            AppUtil.e("An error occurred while trying to read the JSON response from the server", e);
-        }
 
-        jsonString = builder.toString();
+            // build json
+            obj = new JSONObject(builder.toString());
+        } catch (IOException e) {
+            AppUtil.e("An error occurred while trying to read the server's response", e);
+        } catch (JSONException e) {
+            // not a JSONObject, try to build a JSONArray instead
+            try {
+                array = new JSONArray(builder.toString());
+            } catch (JSONException e1) {
+                AppUtil.e("The returned response was invalid JSON", e1);
+            }
+        }
     }
 
     /**
@@ -46,7 +57,7 @@ public class ApiResponse extends Response {
      */
     public String getErrorDetail() {
         try {
-            return new JSONObject(jsonString).getString(ERROR_DETAIL);
+            return obj.getString(ERROR_DETAIL);
         } catch (Exception e) {
             AppUtil.e("An error occurred while trying to read the JSON error from the server", e);
             return null;
@@ -55,19 +66,6 @@ public class ApiResponse extends Response {
 
     @Override
     public Object get() {
-        try {
-            // build json
-            JSONObject obj = null;
-            JSONArray array = null;
-            try {
-                obj = new JSONObject(jsonString);
-            } catch (JSONException e) {
-                array = new JSONArray(jsonString);
-            }
-            return obj != null ? obj : array;
-        } catch (Exception e) {
-            AppUtil.e("An error occurred while reading a JSON response from the server", e);
-            return null;
-        }
+        return obj != null ? obj : array;
     }
 }
